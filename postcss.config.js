8aa8160b58cbac3ded7eedd829b664ed745cce56
bilginkilic <blgnklc@gmail.com>
2025-03-18 12:19:40 +0300

    muze kart

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
 export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer
-  ],
+  plugins: {
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { componentTagger } from 'lovable-tagger';
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -24,4 +21,4 @@ export default defineConfig(({ mode }) => ({
       },
     },
   },
-}));
+});
